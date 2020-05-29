64ef5474220140b6dcc79140b6fc5557ad06fd45
shin.sato <shin.sato@i-studio.co.jp>
2020-05-29 10:56:48 +0900

    add files

---
+++ Mold2/CleanArchitecture.xctemplate/___FILEBASENAME___/Presentation/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___Presenter {
+    weak var view: ___FILEBASENAMEASIDENTIFIER___ViewProtocol!
+    var interactor: ___FILEBASENAMEASIDENTIFIER___InteractorInputProtocol!
+    var router: ___FILEBASENAMEASIDENTIFIER___RouterProtocol!
+    
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___Presenter: ___FILEBASENAMEASIDENTIFIER___PresenterProtocol {
+   
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___Presenter: ___FILEBASENAMEASIDENTIFIER___InteractorOutputProtocol {
+    
+}
---
+++ Mold2/VIPER.xctemplate/___FILEBASENAME___/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+import Foundation
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___ViewController: UIViewController {
+    var presenter: ___FILEBASENAMEASIDENTIFIER___PresenterProtocol!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    deinit {
+        
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___ViewController: ___FILEBASENAMEASIDENTIFIER___ViewProtocol {
+   
+}
